8c42287bfdf0c886539fe31b0e98ea55f054aa03
jorelmagatti <joreltseg@gmail.com>
2023-03-31 01:33:13 -0300

    adicionado base de testes unitarios usando jest supertest falta o restante

---
+++ backend/test/controller/taskService.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+
+const app = require('./../../src/app');
+
+describe('GET /', () => {
+  it('GET / => array of items', () => {
+    return request(app)
+      .get('/tasks')
+
+      .expect('Content-Type', /json/)
+
+      .expect(200)
+
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(Number),
+
+              title: expect.any(String),
+              status: expect.any(String),
+              createat: expect.any(String)
+
+            }),
+          ])
+        );
+      });
+  });
+});
